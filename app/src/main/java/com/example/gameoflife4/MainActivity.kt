39d53f4c53ae9d341131de35b2581f2d4a2bbf9a
Michael Leonard <michael.leonard@mymail.champlain.edu>
2021-11-17 01:21:45 -0500

    finished

--- app/src/main/java/com/example/gameoflife4/MainActivity.kt
+++ app/src/main/java/com/example/gameoflife4/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.gameoflife4
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.provider.DocumentsContract
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -15,9 +15,17 @@ import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.divyanshu.colorseekbar.ColorSeekBar
-import java.util.*
+import com.google.gson.Gson
+import android.R.attr.data
+import android.view.*
+import com.google.gson.reflect.TypeToken
+import java.io.InputStream
+import java.io.OutputStream
+
+const val INTENT_MESSAGE: String = "CopyOfMainActivity"
 
 private var liveColor: Int = Color.WHITE
+private var backColor: Int = Color.BLACK
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -32,15 +40,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainHandler: Handler
     private var paused: Boolean = true
     private lateinit var cellColorBar: ColorSeekBar
+    private lateinit var backgroundColorBar: ColorSeekBar
 
+    private lateinit var saveButton: Button
+    private lateinit var loadButton: Button
+    private lateinit var cloneButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
-        for (i in 0 until totalCells) {
-            items.add(Cell())
-            newItems.add(Cell())
+        if(intent.hasExtra(INTENT_MESSAGE)) {
+            val copiedArray = intent.getBooleanArrayExtra(INTENT_MESSAGE)!!
+            for (i in 0 until totalCells) {
+                items.add(Cell(copiedArray[i]))
+                newItems.add(Cell())
+            }
+        } else {
+            for (i in 0 until totalCells) {
+                items.add(Cell())
+                newItems.add(Cell())
+            }
         }
 
         recyclerView = findViewById(R.id.recycler_view)
@@ -48,6 +71,10 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = CellAdapter(items) { position -> onListItemClick(position) }
 
         generationButton = findViewById(R.id.generationButton)
+
+        saveButton = findViewById(R.id.saveButton)
+        loadButton = findViewById(R.id.loadButton)
+        cloneButton = findViewById(R.id.cloneButton)
 
         mainHandler = Handler(Looper.getMainLooper())
 
@@ -62,6 +89,100 @@ class MainActivity : AppCompatActivity() {
                 recyclerView.adapter?.notifyDataSetChanged()
             }
         })
+        backgroundColorBar = findViewById(R.id.background_color_bar)
+        backgroundColorBar.setOnColorChangeListener(object: ColorSeekBar.OnColorChangeListener{
+            override fun onColorChangeListener(color: Int) {
+                backColor = color
+                recyclerView.adapter?.notifyDataSetChanged()
+            }
+        })
+
+        saveButton.setOnClickListener {
+            createFile(Uri.EMPTY)
+        }
+
+        loadButton.setOnClickListener {
+            openFile(Uri.EMPTY)
+        }
+
+        cloneButton.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra(INTENT_MESSAGE, items.map { c -> c.alive }.toBooleanArray())
+            }
+            startActivity(intent)
+        }
+    }
+
+    // https://developer.android.com/training/data-storage/shared/documents-files
+    // Request code
+    val CREATE_FILE = 1
+    val OPEN_FILE = 2
+
+    private fun createFile(pickerInitialUri: Uri) {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/json"
+            putExtra(Intent.EXTRA_TITLE, "pattern.json")
+
+            // Optionally, specify a URI for the directory that should be opened in
+            // the system file picker before your app creates the document.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        startActivityForResult(intent, CREATE_FILE)
+    }
+
+    private fun openFile(pickerInitialUri: Uri) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/json"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+
+        startActivityForResult(intent, OPEN_FILE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        if(requestCode == CREATE_FILE) {
+            if (data != null) {
+                val gson = Gson()
+                val jsonString = gson.toJson(items)
+
+                val uri = resultData?.data!!
+                val os: OutputStream = baseContext.contentResolver.openOutputStream(uri)!!
+
+                if (os != null) {
+                    os.write(jsonString.toByteArray())
+                    os.close()
+                }
+            }
+        }
+        else if (requestCode == OPEN_FILE) {
+            if (data != null) {
+                val gson = Gson()
+
+                val uri = resultData?.data!!
+                val ins: InputStream = baseContext.contentResolver.openInputStream(uri)!!
+
+                var result = ""
+                if (ins != null) {
+                    result = String(ins.readBytes())
+                    ins.close()
+                }
+
+                val cellListType = object : TypeToken<List<Cell>>() {}.type
+                val newData: MutableList<Cell> = gson.fromJson(result, cellListType)
+                for (i in 0 until totalCells) {
+                    items[i].alive = newData[i].alive
+                }
+                recyclerView.adapter?.notifyDataSetChanged()
+            }
+        }
+
     }
 
     // Help from https://newbedev.com/kotlin-call-a-function-every-second
@@ -144,11 +265,12 @@ class MainActivity : AppCompatActivity() {
                 button.setBackgroundColor(liveColor)
             }
             else {
-                button.setBackgroundColor(Color.rgb(0, 0, 0))
+                button.setBackgroundColor(backColor)
             }
-            /*button.animate().apply {
-                rotationBy(360f)
-            }.start()*/
+
+            button.animate().apply {
+                rotationBy(90f)
+            }.start()
         }
 
         override fun onClick(v: View) {
@@ -169,7 +291,6 @@ class MainActivity : AppCompatActivity() {
             val inflater: LayoutInflater = LayoutInflater.from(parent.context)
             val cellView: View = inflater.inflate(R.layout.cell, parent, false)
             return CellViewHolder(cellView, onItemClicked)
-
         }
 
         override fun onBindViewHolder(holder: CellViewHolder, position: Int) {
